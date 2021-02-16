217ca582378980498100f513e46647014f39b481
Bagoes Rahmaddi Putra <bagoes_rahmaddi@github.com>
2021-02-16 16:31:11 +0700

    Push Tahap 1 - File CI
    Push Tahap 1 - File CI

---
+++ app/Controllers/Beranda_Admin.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controllers;
+
+class Beranda_Admin extends BaseController
+{
+    public function index(){
+        echo view('header_admin');
+        echo view('')
+
+
+
+    }
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
